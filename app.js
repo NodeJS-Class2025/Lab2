94670962c57ecb375d47a3b2499f42e87351f573
bdendro <olegburia2072@gmail.com>
2025-02-23 21:40:57 +0200

    Added 3rd part, removed node_modules

--- app.js
+++ app.js
@@ -26,25 +26,25 @@ app.use('/students', studentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	res.status(404);
-	res.render('error', {
-		title: ':(',
-		message: '404 - Page Not Found',
-	});
+  res.status(404);
+  res.render('error', {
+    title: ':(',
+    message: '404 - Page Not Found',
+  });
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error', {
-		title: ':(',
-		message: 'Error',
-	});
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', {
+    title: ':(',
+    message: 'Error',
+  });
 });
 
 module.exports = app;
--- routes/students.js
+++ routes/students.js
@@ -3,11 +3,43 @@ const router = express.Router();
 
 /* GET students page. */
 router.get('/', function (req, res, next) {
-	res.render('students', {
-		title: 'Students Page',
-		message: 'Welcome to the Students Page!',
-		img: 'images/team.jpg',
-	});
+  res.render('students', {
+    title: 'Students Page',
+    message: 'Welcome to the Students Page!',
+    img: '/images/team.jpg',
+  });
+});
+
+router.get('/oleh', function (req, res) {
+  res.render('students', {
+    title: 'Oleh Page',
+    message: 'Welcome to the Oleh Buriachok Page!',
+    img: '/images/oleh.webp',
+  });
+});
+
+router.get('/vladyslav', function (req, res) {
+  res.render('students', {
+    title: 'Vladyslav Page',
+    message: 'Welcome to the Vladyslav Zhukov Page!',
+    img: '/images/vladyslav.jpg',
+  });
+});
+
+router.get('/maxim', function (req, res) {
+  res.render('students', {
+    title: 'Maxim Page',
+    message: 'Welcome to the Maxim Litynskyi Page!',
+    img: '/images/maxim.webp',
+  });
+});
+
+router.get('/sofia', function (req, res) {
+  res.render('students', {
+    title: 'Sofia Page',
+    message: 'Welcome to the Sofia Tanchuk Page!',
+    img: '/images/sofia.webp',
+  });
 });
 
 module.exports = router;
